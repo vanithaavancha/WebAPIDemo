3d845e98ec660338ad4ec369fb794cd05f96d39d
Vanitha <Vanithaa@adssoft.local>
2018-09-27 14:23:52 +0530

    Add project files.

---
+++ WebAPIDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPIDemo.Models;
+using Newtonsoft.Json;
+
+namespace WebAPIDemo.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        EmployeeDataAccessLayer objemployee = new EmployeeDataAccessLayer();
+        [HttpGet]
+        [Route("api/Employee/Index")]
+        public IEnumerable<Employee> Index()
+        {
+            return objemployee.GetAllEmployees();
+        }
+
+        [HttpPost]
+        [Route("api/Employee/Create")]
+        public int Create([FromBody] Employee employee)
+        {
+            return objemployee.AddEmployee(employee);
+        }
+        [HttpGet]
+        [Route("api/Employee/Details/{id}")]
+        public Employee Details(int id)
+        {
+            return objemployee.GetEmployeeData(id);
+        }
+        [HttpPut]
+        [Route("api/Employee/Edit")]
+        public int Edit([FromBody]Employee employee)
+        {
+            return objemployee.UpdateEmployee(employee);
+        }
+        [HttpDelete]
+        [Route("api/Employee/Delete/{id}")]
+        public int Delete(int id)
+        {
+            return objemployee.DeleteEmployee(id);
+        }
+    }
+}
---
+++ WebAPIDemo/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPIDemo.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Department { get; set; }
+        public string City { get; set; }
+    }
+}
